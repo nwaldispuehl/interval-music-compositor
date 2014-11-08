a67540f431190c90b22187d00a9b64053a40673e
Nico Waldispühl <nw@retorte.ch>
2014-11-08 11:43:08 +0100

    Added default values to compilation parameters model.

--- intervalmusiccompositor.model/src/main/java/ch/retorte/intervalmusiccompositor/compilation/CompilationParameters.java
+++ intervalmusiccompositor.model/src/main/java/ch/retorte/intervalmusiccompositor/compilation/CompilationParameters.java
@@ -7,6 +7,10 @@ import ch.retorte.intervalmusiccompositor.list.ListSortMode;
 import java.util.ArrayList;
 import java.util.List;
 
+import static ch.retorte.intervalmusiccompositor.list.BlendMode.SEPARATE;
+import static ch.retorte.intervalmusiccompositor.list.EnumerationMode.SINGLE_EXTRACT;
+import static ch.retorte.intervalmusiccompositor.list.ListSortMode.SORT;
+
 /**
  * Aggregates all needed compilation parameters.
  * 
@@ -14,13 +18,18 @@ import java.util.List;
  */
 public class CompilationParameters {
 
+  public static final BlendMode DEFAULT_BLEND_MODE = SEPARATE;
+  public static final Double DEFAULT_BLEND_TIME = 1d;
+  public static final EnumerationMode DEFAULT_ENUMERATION_MODE = SINGLE_EXTRACT;
+  public static final ListSortMode DEFAULT_LIST_SORT_MODE = SORT;
+
   public List<Integer> musicPattern = new ArrayList<>();
   public List<Integer> breakPattern = new ArrayList<>();
   public int iterations;
-  public BlendMode blendMode = BlendMode.SEPARATE;
-  public double blendTime;
-  public EnumerationMode enumerationMode = EnumerationMode.SINGLE_EXTRACT;
-  public ListSortMode listSortMode = ListSortMode.SORT;
+  public BlendMode blendMode = DEFAULT_BLEND_MODE;
+  public double blendTime = DEFAULT_BLEND_TIME;
+  public EnumerationMode enumerationMode = DEFAULT_ENUMERATION_MODE;
+  public ListSortMode listSortMode = DEFAULT_LIST_SORT_MODE;
   public String outputPath;
   public String encoderIdentifier;
 
