b3116bde688d22d44c1b1a2945fb913475f3bcd0
Nico Waldispühl <nw@retorte.ch>
2018-11-14 21:13:55 +0100

    OutOfMemoryError when adding long mp3 music file (fixes #51).

--- intervalmusiccompositor.decoder.mp3/src/main/java/ch/retorte/intervalmusiccompositor/decoder/Mp3AudioFileDecoder.java
+++ intervalmusiccompositor.decoder.mp3/src/main/java/ch/retorte/intervalmusiccompositor/decoder/Mp3AudioFileDecoder.java
@@ -1,12 +1,10 @@
 package ch.retorte.intervalmusiccompositor.decoder;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Collection;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 import javazoom.jl.decoder.Bitstream;
@@ -51,9 +49,7 @@ public class Mp3AudioFileDecoder implements AudioFileDecoder {
         audioInputStream.getFormat().getSampleRate(),
         false);
 
-    AudioInputStream result = new DecodedMpegAudioInputStream(decodedFormat, audioInputStream);
-
-    return tidyStream(result);
+    return new DecodedMpegAudioInputStream(decodedFormat, audioInputStream);
   }
 
   /**
@@ -89,81 +85,6 @@ public class Mp3AudioFileDecoder implements AudioFileDecoder {
   @Override
   public Collection<String> getExtensions() {
     return newArrayList(mp3File.getFileExtensions());
-  }
-
-  /**
-   * Creates a new, fresh {@link AudioInputStream} from an existing one. This showed to cure some MP3-related problems.
-   */
-  private AudioInputStream tidyStream(AudioInputStream ais) {
-
-    // TODO: Do we really need this?
-
-    AudioInputStream result = null;
-
-    try {
-      // First read stream into byte array
-      byte[] stream = getByteArrayOfUndefStream(ais);
-
-      // Then create new AudioInputStream from byte array
-      result = new AudioInputStream(new ByteArrayInputStream(stream), ais.getFormat(), AudioSystem.NOT_SPECIFIED);
-    } catch (IOException e) {
-      // nop
-    }
-
-    return result;
-  }
-
-  private byte[] getByteArrayOfUndefStream(AudioInputStream inputStream) throws IOException {
-
-    ArrayList<byte[]> totalBuffer = new ArrayList<>();
-
-    int size = 10240000;
-    byte[] metaBuffer = new byte[size];
-    totalBuffer.add(metaBuffer);
-
-    // Choose a buffer of 100 KB
-    byte[] buffer = new byte[102400];
-
-    int len;
-    int writtenBytes = 0;
-    while ((len = inputStream.read(buffer)) != -1) {
-
-      if ((writtenBytes + len) <= size) {
-
-        System.arraycopy(buffer, 0, metaBuffer, writtenBytes, len);
-        writtenBytes += len;
-
-      } else {
-
-        int remainingBytes = size - writtenBytes;
-        int outstandingBytes = len - remainingBytes;
-
-        // Copy remaining bytes to metaBuffer
-        System.arraycopy(buffer, 0, metaBuffer, writtenBytes, remainingBytes);
-
-        // Change metaBuffer
-        metaBuffer = new byte[size];
-        totalBuffer.add(metaBuffer);
-
-        // Copy rest bytes to new buffer
-        System.arraycopy(buffer, remainingBytes, metaBuffer, 0, outstandingBytes);
-
-        writtenBytes = outstandingBytes;
-      }
-    }
-
-    // Assemble to result array
-    byte[] result = new byte[((totalBuffer.size() - 1) * size) + writtenBytes];
-
-    for (int i = 0; i < totalBuffer.size(); i++) {
-      int bytesToRead = size;
-      if (i == totalBuffer.size() - 1) {
-        bytesToRead = writtenBytes;
-      }
-      System.arraycopy(totalBuffer.get(i), 0, result, size * i, bytesToRead);
-    }
-
-    return result;
   }
 
 }
